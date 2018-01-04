b5f70593da435128b844593815e672609a271f26
Phil <phil.makers@outlook.com>
2018-01-04 15:39:20 +0000

    End of Step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,19 +1,22 @@
 require_relative 'bike'
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
 
-  def initialize
-    @bike = nil
+
+  def initialize(capacity = 20)
+    @capacity = capacity
+    @bikes = []
   end
 
   def release_bike
-    raise "No bike available" unless @bike
-    @bike
+    raise "No bike available" unless @bikes.count > 0
+    @bikes.pop
   end
 
   def dock_bike(bike)
-    raise "Too many bikes" if @bike
-    @bike = bike
+
+    raise "Too many bikes" if @bikes.count == @capacity
+    @bikes << bike
   end
 
 end
