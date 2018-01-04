890a25394a4a9effee74120c9dfb3b9a8fba654e
Phil <phil.makers@outlook.com>
2018-01-04 12:38:01 +0000

    Step 12 - minor refactor

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,11 +4,6 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   it { is_expected.to respond_to(:dock_bike) }
 
-  it 'is working' do
-    #bike = Bike.new
-    expect(Bike.new).to be_working
-  end
-
   it 'bike can be docked' do
     expect(subject).to respond_to(:dock_bike).with(1).argument
   end
