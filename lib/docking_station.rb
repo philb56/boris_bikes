2c7e745294757f412c5f7b2c4444085d8e687857
Antonia Cronin <antoniajcronin@gmail.com>
2018-01-04 15:08:38 +0000

    tested for docking station full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,15 +3,16 @@ class DockingStation
   attr_reader :bike
 
   def initialize
-    @bike = ""
+    @bike = nil
   end
 
   def release_bike
-    raise "No bike available" if @bike.empty?
+    raise "No bike available" unless @bike
     @bike
   end
 
   def dock_bike(bike)
+    raise "Too many bikes" if @bike
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,16 +4,17 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   it { is_expected.to respond_to(:dock_bike) }
 
-  it 'is working' do
-    #bike = Bike.new
-    expect(Bike.new).to be_working
-  end
-
   it 'bike can be docked' do
     expect(subject).to respond_to(:dock_bike).with(1).argument
   end
 
   it 'bike exists' do
     expect{subject.release_bike}.to raise_error("No bike available")
+  end
+  describe '#dock' do
+    it 'raises an error when full' do
+      subject.dock_bike(Bike.new)
+      expect{subject.dock_bike(Bike.new)}.to raise_error("Too many bikes")
+    end
   end
 end
