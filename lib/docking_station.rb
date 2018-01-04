c9b0884764764c59610ef3b032c10356683d9bbc
Antonia Cronin <antoniajcronin@gmail.com>
2018-01-04 16:45:02 +0000

    dealt with merge conflicts

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,14 +8,23 @@ class DockingStation
   end
 
   def release_bike
-    raise "No bike available" unless @bikes.count > 0
+    raise "No bike available" if empty?
     @bikes.pop
   end
 
   def dock_bike(bike)
-
     raise "Too many bikes" if @bikes.count == DEFAULT_CAPACITY
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count == @capacity ? true : false
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
